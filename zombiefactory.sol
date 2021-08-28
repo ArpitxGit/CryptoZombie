0c876dfc36d4ea1578cb75fcf967bcf56d558d5e
ArpitxGit <75042859+ArpitxGit@users.noreply.github.com>
2021-08-29 05:19:37 +0530

    Update zombiefactory.sol

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,15 +1,20 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-contract ZombieFactory {
+import "./ownable.sol";
+
+contract ZombieFactory is Ownable {
 
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
 
     struct Zombie {
-        string name;
-        uint dna;
+      string name;
+      uint dna;
+      uint32 level;
+      uint32 readyTime;
     }
 
     Zombie[] public zombies;
@@ -18,7 +23,7 @@ contract ZombieFactory {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
@@ -32,7 +37,9 @@ contract ZombieFactory {
     function createRandomZombie(string memory _name) public {
         require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
 }
+
